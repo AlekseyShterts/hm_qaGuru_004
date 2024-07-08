3d3da60619b214de7efc506e69f13bbaa60c00e8
AShterts <a.shterts.v@yandex.ru>
2024-07-08 18:33:42 +0300

    Added: 1. build.gradle 2. SearchWikiSelenideTest

---
+++ src/test/java/SearchWikiSelenideTest.java
@@ -0,0 +1,37 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchWikiSelenideTest {
+
+    @BeforeAll
+    public static void ccbeforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void SearchWikiSelenideTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body").find(byText("Soft assertions")).shouldHave(text("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                    @Test
+                    void test() {
+                        Configuration.assertionMode = SOFT;
+                        open("page.html");
+
+                        $("#first").should(visible).click();
+                        $("#second").should(visible).click();
+                    }
+                } 
+                """));
+    }
+}
